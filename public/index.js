0b98474d57b8ee438c9be3178032b2312a6f8fbc
Karabo-ux <111248039+Karabo-ux@users.noreply.github.com>
2025-08-22 21:11:03 +0200

    Update index.js

--- public/index.js
+++ public/index.js
@@ -31,3 +31,23 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+document.getElementById("workerForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent actual form submission
+
+  const toast = document.getElementById("toast");
+  toast.classList.remove("opacity-0");
+  toast.classList.add("opacity-100");
+
+  // Hide toast after 3 seconds
+  setTimeout(() => {
+    toast.classList.remove("opacity-100");
+    toast.classList.add("opacity-0");
+  }, 3000);
+
+  // Optional: Reset the form
+  this.reset();
+  document.getElementById("profilePreview").src = "";
+});
+
+
